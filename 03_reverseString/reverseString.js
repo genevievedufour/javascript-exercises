75c44942500f574ebbd53039803b613cb00e7511
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-07-29 22:51:07 -0400

    3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "";
 
+    let charArray = Array.from(string);
+    let charArrayResult = new Array(charArray.length);
+
+    for(let i = charArray.length-1; i >= 0; i--) {
+        charArrayResult.push(charArray[i]);
+    }
+
+    result = charArrayResult.join('');
+
+    return result;
 };
 
 // Do not edit below this line
