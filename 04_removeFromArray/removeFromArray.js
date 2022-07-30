249bdff10f304261f8a22a54c23c1ece39a02d28
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-07-30 01:03:04 -0400

    Exercice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+
+    const args = [...toRemove];
+
+    let indexesToRemove = new Array();
+
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < args.length; j++)
+        {
+            if(array[i] == args[j] && typeof(array[i]) == typeof(args[j]))
+            {
+                indexesToRemove.push(i)
+            }
+        }
+    }
+
+    for(let i = indexesToRemove.length - 1; i >= 0; i--){
+        array.splice(indexesToRemove[i], 1);
+    }
+
+    return array;
 
 };
 
