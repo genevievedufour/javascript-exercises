168fe9a03f3a7cae43cf6ab08643934d2b6edcb5
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-07-30 23:58:38 -0400

    Exercice 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let result = 0;
+  result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let result = 0;
+  result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for(let i = 0; i < array.length; i++){
+    result += array[i];
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let result = 1;
+  
+  for(let i = 0; i < nums.length; i++){
+    result *= nums[i];
+  }
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+	let result = 1;
+
+  for(let i = 0; i < power; i++){
+    result *= num1
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let result = 1;
+
+  for(let i = 1; i <= num1; i++){
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
