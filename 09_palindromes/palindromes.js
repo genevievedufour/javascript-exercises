ddca9323099ee5b5435f1b9cdb267d9a64fa7052
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-08-19 16:46:29 -0400

    palindromes exercice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+let regex = /\W/g;
+// .!?,
 
+    let isPalindrome = word.toLowerCase().replace(regex, "");
+
+    return isPalindrome.split("").reverse().join("") == isPalindrome;
 };
 
 // Do not edit below this line
