bdd4a3356f78cc8c0c6aa27699e4448f670a9cbf
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-08-19 17:48:56 -0400

    Exercice 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
 
+    for(let i = 0; i < people.length; i++){
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let personAge = getAge( people[i].yearOfBirth, people[i].yearOfDeath);
+
+        if(oldestAge < personAge){
+            oldest = people[i];
+        }
+    }
+
+    return oldest;
 };
+
+function getAge(yearOfBirth, yearOfDeath){
+    if(!yearOfDeath)
+        yearOfDeath = new Date().getFullYear();
+
+    return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
