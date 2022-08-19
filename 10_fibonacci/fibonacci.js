8f3ad8e0f89eddbe43b2f3118ea63854c982373c
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-08-19 17:05:40 -0400

    Fibonacci exercice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0){
+        return "OOPS";
+    }
 
+    if(number === 0){
+    return 0;
+    }
+
+    let fibArray = [];
+
+    for(let i = 0; i <= number; i++){
+        if(i == 0){
+            fibArray[i] = 1;
+        }
+        else if(i == 1){
+            fibArray[i] = 1;
+        }
+        else{
+            fibArray[i] = fibArray[i-1] + fibArray[i-2];
+        }
+    }
+
+    return fibArray[number-1];
 };
 
 // Do not edit below this line
