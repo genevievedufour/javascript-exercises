1d250c7ffe0f23f80b9d44efbc1133e9ebe6d0a2
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-08-19 16:17:31 -0400

    modified calculator with array methods

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,43 +1,22 @@
-const add = function(num1, num2) {
-	let result = 0;
-  result = num1 + num2;
-  return result;
+const add = function(num1, num2) {	
+  return num1 + num2;
 };
 
 const subtract = function(num1, num2) {
-	let result = 0;
-  result = num1 - num2;
-  return result;
+  return num1 - num2;
 };
 
 const sum = function(array) {
-	let result = 0;
-  for(let i = 0; i < array.length; i++){
-    result += array[i];
-  }
-
-  return result;
+  return array.reduce((total, num) => total + num, 0);
 };
 
 const multiply = function(nums) {
-  let result = 1;
-  
-  for(let i = 0; i < nums.length; i++){
-    result *= nums[i];
-  }
-
-  return result;
+  return nums.reduce((total, num) => total * num, 1);
 };
 
 const power = function(num1, power) {
-	let result = 1;
-
-  for(let i = 0; i < power; i++){
-    result *= num1
-  }
-
-  return result;
-};
+    return Math.pow(num1, power);
+}
 
 const factorial = function(num1) {
 	let result = 1;
